30eddc7a1e57fc70154a6dcec9d1f4e836d81ca2
parkchanik <tivas08@gmail.com>
2021-09-22 23:23:30 +0900

    blank

--- temp/script.sql
+++ temp/script.sql
@@ -4,9 +4,9 @@ USE ODIN;
   
  CREATE TABLE CHARACTERS (
 	character_id 	VARCHAR(16) NOT NULL PRIMARY KEY , 
-    character_name 	VARCHAR(20) COMMENT '캐릭터이름',
-    slotinfo 		VARCHAR(100) COMMENT '슬롯 정보 : skill_id , skill_id , ....', 
-    create_time 	TIMESTAMP DEFAULT CURRENT_TIMESTAMP  COMMENT '캐릭터 생성일자'
+	character_name 	VARCHAR(20) COMMENT '캐릭터이름',
+   	slotinfo 		VARCHAR(100) COMMENT '슬롯 정보 : skill_id , skill_id , ....', 
+    	create_time 	TIMESTAMP DEFAULT CURRENT_TIMESTAMP  COMMENT '캐릭터 생성일자'
  );
  
  -- 임시 데이터 삽입
@@ -15,11 +15,11 @@ USE ODIN;
  -- Exception Error 테이블
  CREATE TABLE SP_EXCEPTION_TABLE (
 	idx 		INT NOT NULL AUTO_INCREMENT PRIMARY KEY , 
-	sp_name 	VARCHAR(100) COMMENT '프로시저 이름', 
-    msg 		TEXT COMMENT 'EXECEPTION 텍스트 메시지 ', 
-    sql_state 	VARCHAR(5) COMMENT 'EXCEPTION SQL STATE CODE',
-    parameters 	TEXT COMMENT 'EXCEPTION 당시의 파라미터' ,
-    reg_date 	TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '등록 시간'
+	sp_name 		VARCHAR(100) COMMENT '프로시저 이름', 
+    	msg 		TEXT COMMENT 'EXECEPTION 텍스트 메시지 ', 
+    	sql_state 		VARCHAR(5) COMMENT 'EXCEPTION SQL STATE CODE',
+    	parameters 	TEXT COMMENT 'EXCEPTION 당시의 파라미터' ,
+    	reg_date 		TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '등록 시간'
 );
      
      
